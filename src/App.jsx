166156b2593b6da0bac4e79ba0bf40fdcf55c371
haley-beauchamp <hbeauchamp04@gmail.com>
2025-02-06 08:23:12 -0600

    Added Score, Game Instructions, and Styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,15 @@ import Card from "./components/Card";
 function App() {
 	const cardCount = 12;
 	const [cards, setCards] = useState([]);
-	const [score, setScore] = useState(0);
+	const score = cards.filter((card) => card.clicked).length;
 	const [bestScore, setBestScore] = useState(0);
 
 	useEffect(() => {
 		fetch("https://botw-compendium.herokuapp.com/api/v3/compendium/category/monsters")
 			.then((res) => res.json())
 			.then((data) => {
-				const monters = shuffleArray(data.data).slice(0, cardCount);
-				const selectedMonsters = monters.map((monster) => ({
+				const monsters = shuffleArray(data.data).slice(0, cardCount);
+				const selectedMonsters = monsters.map((monster) => ({
 					name: monster.name,
 					id: monster.id,
 					clicked: false,
@@ -24,14 +24,40 @@ function App() {
 			});
 	}, []);
 
-	function handleCardClick() {
-		
+	function handleCardClick(cardId) {
+		setCards((previousCardState) => {
+			const updatedCards = previousCardState.map((card) => {
+				if (card.id === cardId) {
+					if (card.clicked) {
+						if (score > bestScore) {
+							setBestScore(score);
+						}
+
+						setCards((prevState) => prevState.map((card) => ({ ...card, clicked: false })));
+						return { ...card, clicked: false };
+					}
+
+					return { ...card, clicked: true };
+				}
+
+				return card;
+			});
+
+			return shuffleArray(updatedCards);
+		});
 	}
 
 	return (
 		<>
-			<h1>Zelda Memory Game</h1>
-			<div class='cards'>
+			<div className='align'>
+				<h1>Zelda Memory Game</h1>
+				<div className='scores'>
+					<p>Score: {score}</p>
+					<p>Best Score: {bestScore}</p>
+				</div>
+			</div>
+			<p>Click images to get points! Don't click on any image more than once.</p>
+			<div className='cards'>
 				{cards.map((card) => (
 					<Card key={card.id} name={card.name} image={card.image} onClick={() => handleCardClick(card.id)} />
 				))}
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,14 @@
-export default function Card({name, image, onClick}) {
-    return(
-        <div className='card' onClick={onClick} role="button">
-            <div className="card-content">
-                <img src={image} alt={name}/>
-                <p>{name}</p>
-            </div>
-        </div>
-    )
+import React from "react";
+
+function Card({ name, image, onClick }) {
+	return (
+		<div className='card' onClick={onClick} role='button'>
+			<div className='card-content'>
+				<img src={image} alt='' />
+				<p>{name}</p>
+			</div>
+		</div>
+	);
 }
+
+export default React.memo(Card);
